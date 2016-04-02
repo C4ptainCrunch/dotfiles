be9d5e7e8690846bbbf823882d62e022d14c6f4d
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-02 17:55:54 +0200

    Try again

--- curl.sh
+++ curl.sh
@@ -1,4 +1,4 @@
 # curl -s https://raw.githubusercontent.com/C4ptainCrunch/dotfiles/master/curl.sh | bash -
 
-git clone https://github.com/C4ptainCrunch/dotfiles.git .dotfiles
-.dotfiles/bootstrap.sh -f
+git clone -q https://github.com/C4ptainCrunch/dotfiles.git .dotfiles
+./.dotfiles/bootstrap.sh -f
