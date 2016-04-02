d6ccaa8a75f4073f92b97397644253ffe5d06c02
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-02 18:41:37 +0200

    Change the git star

--- .local/etc/zsh/prompt.zsh
+++ .local/etc/zsh/prompt.zsh
@@ -1,6 +1,6 @@
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' stagedstr '%F{green}●%f'
-zstyle ':vcs_info:*' unstagedstr '%F{yellow}●%f'
+zstyle ':vcs_info:*' stagedstr '%F{green}*%f'
+zstyle ':vcs_info:*' unstagedstr '%F{yellow}*%f'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' enable git hg
 
